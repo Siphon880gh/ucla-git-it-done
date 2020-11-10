78622bf62ae600b0464cdb775e0c2145a1edb016
Weng Fei Fung <weffung@ucdavis.edu>
2020-11-10 05:16:38 -0800

    User interface for fetching user repositories

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,8 @@
+/**
+ * 
+ * @function getUserRepos Get Github user's repositories
+ * @param {string} username 
+ */
 // To Review: Fetch API is not intuitive, but fetching returns a promise, as does response.json(), and you call .then() on promises to use a callback. Most likely why they return promises is the convention of chaining that jQuery AJAX follows.
 var getUserRepos = function(username) {
     // console.log("username", username);
@@ -8,4 +13,33 @@ var getUserRepos = function(username) {
     })
 };
   
-// getUserRepos();
+/**
+ * @function initSearchForm User interface to search Github user
+ * 
+ */
+function initSearchForm() {
+    var userFormEl = document.querySelector("#user-form");
+    var nameInputEl = document.querySelector("#username");
+
+    var formSubmitHandler = function(event) {
+        event.preventDefault();
+        
+        // get value from input element
+        var username = nameInputEl.value.trim();
+        console.log("username input" + username)
+        if (username) {
+
+            getUserRepos(username);
+
+            nameInputEl.value = "";
+
+        } else {
+
+            alert("Please enter a GitHub username");
+
+        }
+    };
+    userFormEl.addEventListener("submit", formSubmitHandler);
+}
+
+initSearchForm();

5f88cfed6ad408bfc87d128f9e6e49753e171555
Weng Fei Fung <weffung@ucdavis.edu>
2020-11-10 05:30:02 -0800

    Simplifying repos response data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,17 +1,38 @@
 /**
  * 
- * @function getUserRepos Get Github user's repositories
+ * @function getUserRepos Get Github user's repositories. An array of objects representing repositories.
  * @param {string} username 
+ * @object {PromiseObject} Obj
+ * @property Obj.name property (for the repository name),
+ * @property Obj.open_issues_count property (for the number of issues)
+ * @property Obj.owner.login property is the repos owner
+ * 
  */
 // To Review: Fetch API is not intuitive, but fetching returns a promise, as does response.json(), and you call .then() on promises to use a callback. Most likely why they return promises is the convention of chaining that jQuery AJAX follows.
 var getUserRepos = function(username) {
     // console.log("username", username);
     fetch(`https://api.github.com/users/${username}/repos`).then(response=>{
         response.json().then(json=>{
-            console.log(json);
+            var arr = json.map(repos=>{
+                const {name, open_issues_count} = repos;
+                const username = repos.owner.login;
+                return {name, open_issues_count, username};
+            });
+            console.log("Repositories simplified:");
+            console.dir(arr);
         })
     })
 };
+
+/**
+ * 
+ * @function displayRepos Display repos information to the user interface
+ * @param {array<string>} repos 
+ * @param {string} searchTerm 
+ */
+var displayRepos = function(repos, searchTerm) {
+
+}
   
 /**
  * @function initSearchForm User interface to search Github user

88c19f7a29caf3652a620fbd369bd01357b4eeee
Weng Fei Fung <weffung@ucdavis.edu>
2020-11-11 04:02:53 -0800

    Added error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,21 +9,31 @@
  * 
  */
 // To Review: Fetch API is not intuitive, but fetching returns a promise, as does response.json(), and you call .then() on promises to use a callback. Most likely why they return promises is the convention of chaining that jQuery AJAX follows.
+// To Review: Fetch API error handling has response.ok to test if success, and has response.statusText
+// To Review: Fetch API error handling informs of
 var getUserRepos = function(username) {
     // console.log("username", username);
-    fetch(`https://api.github.com/users/${username}/repos`).then(response=>{
-        response.json().then(json=>{
-            var arrRepos = json.map(repos=>{
-                const {name, open_issues_count} = repos;
-                const username = repos.owner.login;
-                return {name, open_issues_count, username};
-            });
-            // console.log("Repositories simplified:");
-            // console.dir(arrRepos);
-            displayRepos(arrRepos, username);
+    fetch(`https://api.github.com/users/${username}/repos`)
+        .then(response => {
+            if (response.ok)
+                return response.json();
+            else
+                alert("Error: " + response.statusText)
+        }).then(json => {
+            if (json) {
+                var arrRepos = json.map(repos => {
+                    const { name, open_issues_count } = repos;
+                    const username = repos.owner.login;
+                    return { name, open_issues_count, username };
+                });
+                // console.log("Repositories simplified:");
+                // console.dir(arrRepos);
+                displayRepos(arrRepos, username);
+            }
+        }).catch(error => {
+            alert("Error: Unable to connect to Github. Reason: " + error);
         });
-    })
-};
+}
 
 /**
  * 
@@ -36,8 +46,13 @@ var displayRepos = function(repos, searchTerm) {
         repo-search-term
         repos-container
     */
+
     var repoContainerEl = document.querySelector("#repos-container");
     var repoSearchTerm = document.querySelector("#repo-search-term");
+
+    if (repos.length == 0) {
+        repoContainerEl.textContent = "No repositories found";
+    }
 
     // clear old content
     repoContainerEl.textContent = "";
@@ -46,52 +61,52 @@ var displayRepos = function(repos, searchTerm) {
 
     for (var i = 0; i < repos.length; i++) {
 
-    // format repo name
+        // format repo name
 
-    var repoName = repos[i].username + "/" + repos[i].name;
-
-
-    // create a container for each repo
-
-    var repoEl = document.createElement("div");
-
-    repoEl.classList = "list-item flex-row justify-space-between align-center";
+        var repoName = repos[i].username + "/" + repos[i].name;
 
 
-    // create a span element to hold repository name
+        // create a container for each repo
 
-    var titleEl = document.createElement("span");
+        var repoEl = document.createElement("div");
 
-    titleEl.textContent = repoName;
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
 
-    // append to container
-    repoEl.appendChild(titleEl);
 
-    // create a status element
+        // create a span element to hold repository name
 
-    var statusEl = document.createElement("span");
+        var titleEl = document.createElement("span");
 
-    statusEl.classList = "flex-row align-center";
+        titleEl.textContent = repoName;
 
-    // check if current repo has issues or not
-    if (repos[i].open_issues_count > 0) {
-        statusEl.innerHTML =  "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-    } else {
-        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
+        // append to container
+        repoEl.appendChild(titleEl);
 
-    // append to container
+        // create a status element
 
-    repoEl.appendChild(statusEl);
+        var statusEl = document.createElement("span");
 
-    // append container to the dom
-    repoContainerEl.appendChild(repoEl);
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append to container
+
+        repoEl.appendChild(statusEl);
+
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
 
     } // for
 
 
 }
-  
+
 /**
  * @function initSearchForm User interface to search Github user
  * 
@@ -102,7 +117,7 @@ function initSearchForm() {
 
     var formSubmitHandler = function(event) {
         event.preventDefault();
-        
+
         // get value from input element
         var username = nameInputEl.value.trim();
         console.log("username input" + username)
